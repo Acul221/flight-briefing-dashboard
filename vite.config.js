91e39a87533178b9d00ccc88fc05dddbf44359f1
Hasrullah Azis <hasrullahazis@email.com>
2025-05-02 05:08:47 +0700

    fix: remove sourcemaps and define HMR fallback

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    __HMR_CONFIG_NAME__: JSON.stringify(''),
-  },
   build: {
     sourcemap: false,
   },
