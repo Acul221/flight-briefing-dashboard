21f6652b68323f3af49801c42d1c056b1224cd6c
Hasrullah Azis <hasrullahazis@email.com>
2025-05-02 07:07:46 +0700

    fix: define HMR & global variables for Netlify build

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,13 @@ export default defineConfig({
   define: {
     __DEFINES__: {},
     __HMR_CONFIG_NAME__: JSON.stringify(''),
-    __BASE__: JSON.stringify('/'), // ✅ tambahkan ini
+    __BASE__: JSON.stringify('/'),
+    __SERVER_HOST__: JSON.stringify(''),
+    __HMR_PROTOCOL__: JSON.stringify(''),
+    __HMR_PORT__: JSON.stringify(''),
+    __HMR_HOSTNAME__: JSON.stringify(''),
+    __HMR_DIRECT_TARGET__: JSON.stringify(''),
+    __HMR_TIMEOUT__: JSON.stringify(30000),
+    __WS_TOKEN__: JSON.stringify(''),
   },
 });
